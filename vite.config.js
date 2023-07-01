5eb2ce53b1decc04aa5cbe28a5602fc94890aa4b
Rémi <contact@remipouly.fr>
2023-07-01 14:57:49 +0200

    Ajout du CV en resource vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 "resources/scss/app.scss",
                 "resources/js/app.js",
                 "resources/scss/home/index.scss",
+                "resources/pdf/CV_POULY_REMI.pdf",
             ],
             refresh: true,
         }),
