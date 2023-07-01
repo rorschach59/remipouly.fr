b1c7490eb53d59a68852ac3d81f49481ce0279fc
Rémi <contact@remipouly.fr>
2023-07-01 16:47:51 +0200

    Style du mail envoyé au prospect

--- resources/views/emails/contact-client.blade.php
+++ resources/views/emails/contact-client.blade.php
@@ -4,13 +4,12 @@
     J'ai bien noté votre demande à laquelle je donnerai suite dans les meilleurs délais.
 </p>
 <p>Bien à vous</p>
-<div>
+<div style="text-align: center; margin-top: 4em">
     <a href="https://www.remipouly.fr">
-        <img src="https://remipouly.fr/resources/images/logo-pouly-remi-developpeur-freelance.png"
+        <img src="{{ Vite::asset('resources/images/logo-pouly-remi-developpeur-freelance.png') }}"
              alt="Logo de POULY Rémi - Développeur web freelance"
-             width="150px" height="150px"
+             width="250" height="100"
         >
     </a>
+    <p><a href="https://www.remipouly.fr" style="text-decoration: underline">https://www.remipouly.fr</a></p>
 </div>
-
-<p><a href="https://remipouly.fr" style="text-decoration: underline">https://remipouly.fr</a></p>
