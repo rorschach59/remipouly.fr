2c583d7690468ce55c4ccc361dc251d33ad4716f
reimpo <remi.p@hiflow.com>
2025-02-10 21:08:40 +0100

    master : modification de la planification

--- app/app/Console/Kernel.php
+++ app/app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('check:email-address')->dailyAt('08:30');
     }
 
     /**
--- app/routes/console.php
+++ app/routes/console.php
@@ -1,11 +1,18 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('check-email-address')
-    ->dailyAt('08:30');
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
